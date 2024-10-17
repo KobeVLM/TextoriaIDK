3cfb8e3315a67144e3956f6c89963b728d48e7c1
Kyube <kobemarikit70@gmail.com>
2024-10-17 10:14:43 +0800

    Add Weather class to handle weather-related functionality Add CharacterTest and HeroTest classes for unit testing Update project settings to include test paths and referenced libraries

---
+++ test/characters/HeroTest.java
@@ -0,0 +1,11 @@
+package characters;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class HeroTest {
+    @Test
+    public void testHeroExists() {
+        assertTrue(true);
+    }
+}
